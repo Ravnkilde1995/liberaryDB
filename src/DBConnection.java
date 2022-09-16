fad2fbd004712c495f852318e33106dba7058a21
Oliver Ravnkilde <ravnkilde1995@gmail.com>
2022-09-16 12:58:14 +0200

    Establed Connection

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,4 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
 
+    public static Connection connection;
+
+    public static Connection createConnection() {
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/bibliotek?serverTimezone=CET&useSSL=false", "root", "ravn123");
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+
+        }
+
+        return connection;
+    }
 
 }
